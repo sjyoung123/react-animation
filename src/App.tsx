3500afd850a496c3c34461270d1594feafa8defd
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2021-12-25 20:12:19 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,72 +6,71 @@ const Wrapper = styled(motion.div)`
   height: 100vh;
   width: 100vw;
   display: flex;
-  flex-direction: column;
-  justify-content: center;
+  justify-content: space-around;
   align-items: center;
+`;
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  width: 50vw;
+  gap: 10px;
+  div:first-child,
+  div:last-child {
+    grid-column: span 2;
+  }
 `;
 
 const Box = styled(motion.div)`
-  width: 500px;
-  height: 300px;
+  height: 200px;
   background-color: ghostwhite;
-  position: absolute;
   display: flex;
   align-items: center;
   justify-content: center;
-  font-size: 30px;
-  top: 80px;
+  border-radius: 15px;
 `;
 
-const Btn = styled.button``;
+const Overlay = styled(motion.div)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  background-color: rgba(0, 0, 0, 0.5);
+`;
 
-const boxVariants: Variants = {
-  initial: (isBack: boolean) => {
-    return { x: isBack ? -500 : 500, opacity: 0, scale: 0 };
-  },
-  animate: { x: 0, opacity: 1, scale: 1, transition: { duration: 1 } },
-  exit: (isBack) => {
-    return {
-      x: isBack ? 500 : -500,
-      opacity: 0,
-      scale: 0,
-      transition: { duration: 1 },
-    };
-  },
+const overlay: Variants = {
+  initial: { backgroundColor: "rgba(0, 0, 0, 0)" },
+  animate: { backgroundColor: "rgba(0, 0, 0, 0.5)" },
+  exit: { backgroundColor: "rgba(0, 0, 0, 0)" },
 };
 
 function App() {
-  const [showing, setShowing] = useState(1);
-  const [isBack, setIsBack] = useState(false);
-
-  const onNext = () => {
-    setIsBack(false);
-    setShowing((data) => (data === 10 ? 10 : data + 1));
-  };
-
-  const onPrev = () => {
-    setIsBack(true);
-    setShowing((data) => (data === 1 ? 1 : data - 1));
-  };
+  const [id, setId] = useState<null | string>(null);
 
   return (
     <Wrapper>
-      <AnimatePresence custom={isBack}>
-        <Box
-          custom={isBack}
-          key={showing}
-          variants={boxVariants}
-          initial="initial"
-          animate="animate"
-          exit="exit"
-        >
-          {showing}
-        </Box>
+      <Grid>
+        {[1, 2, 3, 4].map((n) => {
+          return (
+            <Box onClick={() => setId(n + "")} key={n} layoutId={n + ""} />
+          );
+        })}
+      </Grid>
+      <AnimatePresence>
+        {id ? (
+          <Overlay
+            onClick={() => setId(null)}
+            variants={overlay}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+          >
+            <Box layoutId={id} style={{ width: "400px" }} />
+          </Overlay>
+        ) : null}
       </AnimatePresence>
-      <div style={{ marginTop: "150px" }}>
-        <Btn onClick={onPrev}>Prev</Btn>
-        <Btn onClick={onNext}>Next</Btn>
-      </div>
     </Wrapper>
   );
 }
